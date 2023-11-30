8ac0c96b9ca9cc8f463631872b2f7f87b3b6d813
Suran <skularatna@gmail.com>
2023-11-30 15:46:34 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "",
-    host: "",
-    database: "",
-    password: "",
+    user: "postgres",
+    host: "apprunner-test.cluster-czypxssauleq.ap-southeast-1.rds.amazonaws.com",
+    database: "apprunner-db",
+    password: "eDYIgEkTON×Mzyj8mQCk",
     port: 5432
 });
 
